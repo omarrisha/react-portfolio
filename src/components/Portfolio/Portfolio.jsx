92135258b6296334bc6326833949ef27a99f8af8
omarrisha <omarrisha1r@hotmail.com>
2023-01-13 20:15:59 +0100

    Update Portfolio.jsx

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -9,16 +9,15 @@ const Portfolio = () => {
       <h2 className="section__title">What have I done so far?</h2>
       <div className="work__container grid">
         {items.map((item) => {
-          const { id, image, title, category } = item;
+          const { id, image, title, url } = item;
           return (
             <div className="work__card paper-box" key={id}>
               <div className="work__thumnail">
                 <img src={image} alt="" className="work__img" />
                 <div className="work__mask"></div>
               </div>
-              <span className="work__category">{category}</span>
               <h3 className="work__title">{title}</h3>
-              <a href="#" className="work__button">
+              <a href={url} className="work__button" target="_blank">
                 <i className="icon-link work__button-icon"></i>
               </a>
             </div>
