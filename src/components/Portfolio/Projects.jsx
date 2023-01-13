f5fe145ee6929b481d5060e27c3072a577cd9d8c
omarrisha <omarrisha1r@hotmail.com>
2023-01-13 20:16:04 +0100

    Update Projects.jsx

--- src/components/Portfolio/Projects.jsx
+++ src/components/Portfolio/Projects.jsx
@@ -1,46 +1,25 @@
-import Project1 from "../../assets/images/work-1.svg";
-import Project2 from "../../assets/images/work-2.svg";
-import Project3 from "../../assets/images/work-3.svg";
-import Project4 from "../../assets/images/work-4.svg";
-import Project5 from "../../assets/images/work-5.svg";
-import Project6 from "../../assets/images/work-6.svg";
+import Project1 from "../../assets/images/chromeextensiontime.png";
+import Project2 from "../../assets/images/gameofwar.png";
+import Project3 from "../../assets/images/tenzies.png";
 
 const Projects = [
   {
     id: 1,
     image: Project1,
-    title: "Project 1",
-    category: "React",
+    title: "Chrome Extensions Time",
+    url: "https://aquamarine-khapse-c5ed68.netlify.app/",
   },
   {
     id: 2,
     image: Project2,
-    title: "Project 2",
-    category: "React",
+    title: "Game of War",
+    url: "https://loquacious-sfogliatella-be0712.netlify.app/",
   },
   {
     id: 3,
     image: Project3,
-    title: "Project 3",
-    category: "React",
-  },
-  {
-    id: 4,
-    image: Project4,
-    title: "Project 4",
-    category: "React",
-  },
-  {
-    id: 5,
-    image: Project5,
-    title: "Project 5",
-    category: "React",
-  },
-  {
-    id: 6,
-    image: Project6,
-    title: "Project 6",
-    category: "React",
+    title: "Tenzies Game",
+    url: "https://melodious-kataifi-b19d76.netlify.app/",
   },
 ];
 
