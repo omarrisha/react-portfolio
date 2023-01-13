5c5af89d5372f5171a665e619ae49707de4c2f8c
omarrisha <omarrisha1r@hotmail.com>
2023-01-13 20:32:18 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Sidebar, Home, About, Portfolio, Contact } from "./components";
+import { Home, About, Portfolio, Contact } from "./components";
 
 function App() {
   return (
